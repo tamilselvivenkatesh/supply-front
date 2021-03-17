eba5937eb40fab8bab84d0ac3e38566648f8c4d1
Tamil selvi <tamilselvivenky99@gmail.com>
2021-03-17 16:23:27 +0530

    first commit

---
+++ precache-manifest.b2526761663a624939dd8788d7fc1ad6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84b184b285fc22f88a69",
+    "url": "/static/css/main.b5b27c40.chunk.css"
+  },
+  {
+    "revision": "84b184b285fc22f88a69",
+    "url": "/static/js/main.84b184b2.chunk.js"
+  },
+  {
+    "revision": "f0facc6b68d8dbc43898",
+    "url": "/static/js/1.f0facc6b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65981811d56fc66370de22e057584cac",
+    "url": "/index.html"
+  }
+];
